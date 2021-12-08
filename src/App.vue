4e2bc2dc50d7a78d6dbc54490c56ddaff8db8578
eraybulut34 <abdullaheraybulut@hotmail.com>
2021-12-08 20:09:16 +0300

    arrangement

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Covid19 msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Covid19 from './components/Covid19.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Covid19
   }
 }
 </script>
